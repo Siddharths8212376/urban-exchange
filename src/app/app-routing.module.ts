f1b12dc0cc866c9418e7d4415f1ed50be0e2904d
aydhar <aydhar@cisco.com>
2023-11-05 18:47:44 +0530

    edit profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './components/home/home.component';
 
 import { LoginComponent } from './authentication/login/login.component';
 import { SignupComponent } from './authentication/signup/signup.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'profile/:id', component: UserProfileComponent },
   { path: 'product', loadChildren: () => import('./product/product.module').then(module => module.ProductModule) },
   { path: '**', redirectTo: '/home' }
 ]
