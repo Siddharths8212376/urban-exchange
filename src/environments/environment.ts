2016c978e5890f862d6004a108f40fe10e7f7b53
Siddharths8212376 <siddharth8212376@gmail.com>
2023-12-11 21:27:02 +0530

    update env file - for deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,12 @@
+let apiUrl = ''
+switch (window.location.hostname) {
+    case "localhost":
+        apiUrl = "http://localhost:5000/api"
+        break;
+    case "urban-exchange.onrender.com":
+        apiUrl = "https://urban-exchange.onrender.com/api"
+        break;
+}
 export const env = {
-    apiUrl: "http://localhost:5000/api"
+    apiUrl
 }
