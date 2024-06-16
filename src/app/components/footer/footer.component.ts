2f1e5af8b178e570b36b2d96bdb2ecc3740971bf
Siddharths8212376 <siddharth8212376@gmail.com>
2024-06-16 14:51:39 +0530

    fix issues, change styles for login/signup

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationStart, Router, RouterEvent } from '@angular/router';
+import { AuthService } from 'src/app/authentication/auth.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-
+export class FooterComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+  isOnLoginSignUpPage: boolean = false;
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        if (event.url && (event.url.includes('login') || event.url.includes('signup')) && !this.authService.getIsAuth()) {
+          this.isOnLoginSignUpPage = true;
+        } else {
+          this.isOnLoginSignUpPage = false;
+        }
+      }
+    })
+  }
 }
--- src/app/product/display-product/display-product.component.ts
+++ src/app/product/display-product/display-product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/authentication/auth.service';
 import { defaultProduct } from 'src/app/constants/product.constant';
 import { Product } from 'src/app/models/product.model';
@@ -16,7 +16,7 @@ import { defaultUser } from 'src/app/constants/user.constant';
 @Component({
   selector: 'app-display-product',
   templateUrl: './display-product.component.html',
-  styleUrls: ['./display-product.component.scss']
+  styleUrls: ['./display-product.component.scss'],
 })
 export class DisplayProductComponent implements OnInit {
   product: Product = defaultProduct;
@@ -40,53 +40,70 @@ export class DisplayProductComponent implements OnInit {
     private imageService: ImageService,
     private sanitizer: DomSanitizer,
     public dialog: MatDialog,
-    public authService: AuthService
+    public authService: AuthService,
+    private router: Router
   ) { }
   async ngOnInit(): Promise<void> {
-    this.route.params.subscribe(param => {
+    this.route.params.subscribe((param) => {
       this.productId = this.route.snapshot.paramMap.get('id');
       this.getProductDetails();
-
-    })
-    this.currUser = this.authService.getCurrentUser() ? this.authService.getCurrentUser() : '';
+    });
+    this.currUser = this.authService.getCurrentUser()
+      ? this.authService.getCurrentUser()
+      : '';
     this.productId = this.route.snapshot.paramMap.get('id');
     this.wishlist = await this.userService.getWislist();
     this.isInWishlist = this.wishlist.includes(this.productId);
   }
   getProductDetails() {
     this.imageFiles = [];
-    this.productService.getProductById(this.productId).subscribe(response => {
+    this.productService.getProductById(this.productId).subscribe((response) => {
       this.product = response.data;
       this.productImages = this.product.productImages;
-      if (this.product.address != undefined || this.product.address != null) this.productLocation = this.product.address.location.coordinates;
+      if (this.product.address != undefined || this.product.address != null)
+        this.productLocation = this.product.address.location.coordinates;
       this.entries = Object.entries(this.product);
       if (this.productImages.length > 0) {
-        this.productImages.forEach(imageName => this.imageService.getImageByName(imageName).subscribe(image => {
-          let objectURL = URL.createObjectURL(image);
-          this.imageFiles.push(this.sanitizer.bypassSecurityTrustUrl(objectURL));
-        }, (error) => {
-          console.error(error);
-          // try cloud images
-          this.imageService.getImageURLByName(imageName).subscribe(response => {
-            if (response.data) {
-              this.imageFiles.push(response.data.secureUrl);
-            } else {
-              this.imageFiles.push('../../assets/images/no-image.svg');
+        this.productImages.forEach((imageName) =>
+          this.imageService.getImageByName(imageName).subscribe(
+            (image) => {
+              let objectURL = URL.createObjectURL(image);
+              this.imageFiles.push(
+                this.sanitizer.bypassSecurityTrustUrl(objectURL)
+              );
+            },
+            (error) => {
+              console.error(error);
+              // try cloud images
+              this.imageService
+                .getImageURLByName(imageName)
+                .subscribe((response) => {
+                  if (response.data) {
+                    this.imageFiles.push(response.data.secureUrl);
+                  } else {
+                    this.imageFiles.push('../../assets/images/no-image.svg');
+                  }
+                });
             }
-          });
-        }))
+          )
+        );
       } else {
         this.imageFiles.push('../../assets/images/no-image.svg');
       }
-    })
+    });
   }
   getProductLocation(product: Product): string {
     let loc = product.address?.meta[0];
-    return loc ? `${loc.postalLocation}, ${loc.district}, ${loc.state}` : 'Bangalore';
+    return loc
+      ? `${loc.postalLocation}, ${loc.district}, ${loc.state}`
+      : 'Bangalore';
   }
   async clickOnWishlist() {
+    if (!this.authService.getIsAuth()) {
+      this.router.navigate(['login']);
+      return;
+    }
     this.isInWishlist = !this.isInWishlist;
-
     if (this.isInWishlist) this.wishlist.unshift(this.productId);
     else {
       let productIndex = this.wishlist.indexOf(this.productId);
@@ -96,46 +113,50 @@ export class DisplayProductComponent implements OnInit {
     }
 
     this.loader.start();
-    await this.userService.addDeleteFromUserWishlist(this.wishlist).subscribe(response => {
-      this.loader.stop();
-    });
+    await this.userService
+      .addDeleteFromUserWishlist(this.wishlist)
+      .subscribe((response) => {
+        this.loader.stop();
+      });
   }
 
   openChat(seller: any) {
-
-    let currentUser = this.authService.getCurrentUser() ? this.authService.getCurrentUser() : '';
-
-    this.productService.getChatId(currentUser, this.productId, seller).subscribe((response: any) => {
-
-      //if response is 404 then cr
-
-
-
-      if (response && response.message !== 'Chat not found') {
-        let data = {
-          chatData: response.message,
-          partner: seller,
-        }
-        this.openChatInterface(data);
-
-      } else {
-        let tempUser = defaultUser; tempUser._id = seller;
-        this.userService.userDetails(tempUser).subscribe(sellerInfo => {
-          this.productService.createChat(currentUser, this.productId, sellerInfo).subscribe(response => {
-            let data = {
-              chatData: response.data,
-              partner: seller,
-            }
-
-            this.openChatInterface(data);
-          });
-        })
-
-      }
+    if (!this.authService.getIsAuth()) {
+      // redirect login
+      this.router.navigate(['login']);
+      return;
     }
-    );
+    let currentUser = this.authService.getCurrentUser()
+      ? this.authService.getCurrentUser()
+      : '';
+    this.productService
+      .getChatId(currentUser, this.productId, seller)
+      .subscribe((response: any) => {
+        //if response is 404 then cr
 
+        if (response && response.message !== 'Chat not found') {
+          let data = {
+            chatData: response.message,
+            partner: seller,
+          };
+          this.openChatInterface(data);
+        } else {
+          let tempUser = defaultUser;
+          tempUser._id = seller;
+          this.userService.userDetails(tempUser).subscribe((sellerInfo) => {
+            this.productService
+              .createChat(currentUser, this.productId, sellerInfo)
+              .subscribe((response) => {
+                let data = {
+                  chatData: response.data,
+                  partner: seller,
+                };
 
+                this.openChatInterface(data);
+              });
+          });
+        }
+      });
   }
 
   openChatInterface(data: any) {
@@ -143,7 +164,7 @@ export class DisplayProductComponent implements OnInit {
       width: '30%',
       height: '70%',
       minWidth: '40rem',
-      data: data  // Pass the seller data to your dialog component
+      data: data, // Pass the seller data to your dialog component
     });
   }
 
